3982d8f60ed17fbface4e1f4c81f7f62df5b8f77
Nahin05 <73245183+Nahin05@users.noreply.github.com>
2020-12-02 11:52:15 -0500

    Add files via upload

---
+++ Taller3/taller3-Nahin05/Ejercicio05/src/ejercicio05/Ejercicio05.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio05;
+
+/**
+ *
+ * @author Elio Sivizaca
+ */
+public class Ejercicio05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double resustados = (Math.pow(2,2)+12-1)/3;
+        System.out.println((Math.pow(2,2)+12-1)/3);
+        
+    }
+    
+}
